d2d472487b9bdf8f4352700cece64dd306ee518f
jomorales1 <jomorales@unal.edu.co>
2023-03-31 15:10:53 -0500

    Add logging files

--- routes/scheduler.py
+++ routes/scheduler.py
@@ -4,7 +4,9 @@ from celery import shared_task
 from configparser import ConfigParser
 from datetime import datetime, timedelta
 
+from email import encoders
 from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 
 import os
@@ -29,6 +31,7 @@ parser.add_argument('course_id', type=int, required=True, help='Course id is req
 # Defining constants
 MAX_RETRIES = int(params.get('Alcala', 'max_retries'))
 DATABASE = cd + '/../alcala.db'
+LOGS_PATH = cd + '/../logs'
 
 # Setting up database
 try:
@@ -94,6 +97,16 @@ def notify_admin(tuition_id):
         content = html_file.read()
         content = content.replace('{{tuition_id}}', str(tuition_id))
         msg.attach(MIMEText(content, 'html'))
+    # Include log file
+    attachment_location = LOGS_PATH + f'/{str(tuition_id)}.log'
+    filename = os.path.basename(attachment_location)
+    attachment = open(attachment_location, 'rb')
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload(attachment.read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', f'attachment; filename= {filename}')
+    msg.attach(part)
+    # Send message
     try:
         server = smtplib.SMTP('smtp.office365.com', 587)
         server.starttls()
@@ -118,37 +131,76 @@ def check_tuition_status(tuition_id, course_id, prev_attempts):
         If after MAX_RETRIES attempts tuition status is `pending` stop scheduling
         tasks and notify admin.
     """
-    # Check if there is a valid access_token in the SQLite database
-    access_token = ''
-    conn = sqlite3.connect(DATABASE)
-    cursor = conn.cursor()
-    try:
-        res = cursor.execute('SELECT token, expiration_date FROM access_token')
-        data = res.fetchone()
-        if data is not None:
-            current_date = datetime.now()
-            expiration_date = datetime.strptime(data[1], '%Y-%m-%dT%H:%M:%S') - timedelta(minutes=5)
-            if current_date < expiration_date:
-                access_token = data[0]
-            else:
-                cursor.execute('DELETE from access_token WHERE token = ?', (data[0],))
-                conn.commit()
-    except Exception as error:
-        conn.close()
-        print(f'Error while reading access token from database: {str(error)}')
-        return
-    # Request access_token if not found previously
-    try:
+    with open(LOGS_PATH + f'/{str(tuition_id)}.log', 'a+', encoding='utf-8') as log_file:
+        log_file.write(f'Task #{str(prev_attempts + 1)}\n')
+        log_file.write(f'Date: {datetime.strftime(datetime.utcnow(), "%Y-%m-%dT%H:%M:%S")}\n')
+        # Check if there is a valid access_token in the SQLite database
+        access_token = ''
+        conn = sqlite3.connect(DATABASE)
+        cursor = conn.cursor()
+        try:
+            res = cursor.execute('SELECT token, expiration_date FROM access_token')
+            data = res.fetchone()
+            if data is not None:
+                current_date = datetime.now()
+                expiration_date = datetime.strptime(data[1], '%Y-%m-%dT%H:%M:%S') - timedelta(minutes=5)
+                if current_date < expiration_date:
+                    access_token = data[0]
+                else:
+                    cursor.execute('DELETE from access_token WHERE token = ?', (data[0],))
+                    conn.commit()
+        except Exception as error:
+            conn.close()
+            print(f'Error while reading access token from database (tuition_id = {str(tuition_id)}): {str(error)}')
+        # Request access_token if not found previously
         if not access_token:
-            at_request = requests.post(alcala_url + '/oauth/token', json={
-                'grant_type': 'client_credentials',
-                'client_id': client_id,
-                'client_secret': client_secret
-            })
-            # If request failed shedule next task
-            if not at_request.ok:
+            try:
+                at_request = requests.post(alcala_url + '/oauth/token', json={
+                    'grant_type': 'client_credentials',
+                    'client_id': client_id,
+                    'client_secret': client_secret
+                })
+                log_file.write(f'Access token response: [{str(at_request.status_code)}]\n{str(at_request.text)}\n')
+                # If request failed shedule next task
+                if not at_request.ok:
+                    conn.close()
+                    print(f'Error while requesting access_token: [{str(at_request.status_code)}] {str(at_request.text)}')
+                    # Check if max attempts were reached
+                    if prev_attempts < MAX_RETRIES - 1:
+                        print('Scheduling next task...')
+                        exec_date = datetime.utcnow() + timedelta(minutes=3)
+                        check_tuition_status.apply_async((tuition_id, course_id, prev_attempts + 1), eta=exec_date)
+                    else:
+                        print(f'Max retries exceeded ({str(prev_attempts + 1)}) with tuition_id {str(tuition_id)}')
+                        notify_admin(tuition_id)
+                    return
+                # Process response and save new access_token
+                at_response = json.loads(at_request.text)
+                access_token = at_response['access_token']
+                expiration_date = datetime.now() + timedelta(seconds=int(at_response['expires_in']))
+                cursor.execute('INSERT INTO access_token VALUES (?, ?)', (access_token, datetime.strftime(expiration_date, '%Y-%m-%dT%H:%M:%S')))
+                conn.commit()
+            except Exception as error:
                 conn.close()
-                print(f'Error while requesting access_token: [{str(at_request.status_code)}] {str(at_request.text)}')
+                print(f'Error while requesting access token (tuition_id = {str(tuition_id)}): {str(error)}')
+                log_file.write(f'Error while requesting access token: {str(error)}\n')
+                notify_admin(tuition_id)
+                return
+        conn.close()
+        # Request tuition data
+        try:
+            tuition_request = requests.get(alcala_url + f'/matriculas/{str(tuition_id)}', headers={
+                'Authorization': f'Bearer {access_token}'
+            })
+            log_file.write(f'Access token response: [{str(tuition_request.status_code)}]\n{str(tuition_request.text)}\n')
+            # If status code is 404 not found, stop scheduling tasks
+            if tuition_request.status_code == 404:
+                print(f'Tuition not found. Next task will not be scheduled')
+                return
+            # If request failed shedule next task
+            if not tuition_request.ok:
+                conn.close()
+                print(f'Error while requesting tuition data: [{str(tuition_request.status_code)}] {str(tuition_request.text)}')
                 # Check is max attempts were reached
                 if prev_attempts < MAX_RETRIES - 1:
                     print('Scheduling next task...')
@@ -158,30 +210,16 @@ def check_tuition_status(tuition_id, course_id, prev_attempts):
                     print(f'Max retries exceeded ({str(prev_attempts + 1)}) with tuition_id {str(tuition_id)}')
                     notify_admin(tuition_id)
                 return
-            # Process response and save new access_token
-            at_response = json.loads(at_request.text)
-            access_token = at_response['access_token']
-            expiration_date = datetime.now() + timedelta(seconds=int(at_response['expires_in']))
-            cursor.execute('INSERT INTO access_token VALUES (?, ?)', (access_token, datetime.strftime(expiration_date, '%Y-%m-%dT%H:%M:%S')))
-            conn.commit()
-    except Exception as error:
-        conn.close()
-        print(f'Error while requesting access token: {str(error)}')
-        return
-    conn.close()
-    # Request tuition data
-    try:
-        tuition_request = requests.get(alcala_url + f'/matriculas/{str(tuition_id)}', headers={
-            'Authorization': f'Bearer {access_token}'
-        })
-        # If status code is 404 not found, stop scheduling tasks
-        if tuition_request.status_code == 404:
-            print(f'Tuition not found. Next task will not be scheduled')
+            # Process response
+            tuition_data = json.loads(tuition_request.text)
+            print(json.dumps(tuition_data, indent=4))
+        except Exception as error:
+            print(f'Error while requesting tuition data (tuition_id = {str(tuition_id)}): {str(error)}')
+            log_file.write(f'Error while requesting tuition data: {str(error)}\n')
+            notify_admin(tuition_id)
             return
-        # If request failed shedule next task
-        if not tuition_request.ok:
-            conn.close()
-            print(f'Error while requesting tuition data: [{str(tuition_request.status_code)}] {str(tuition_request.text)}')
+        # Check tuition status
+        if tuition_data['data']['estado_matricula'] == 'pendiente':
             # Check is max attempts were reached
             if prev_attempts < MAX_RETRIES - 1:
                 print('Scheduling next task...')
@@ -190,32 +228,20 @@ def check_tuition_status(tuition_id, course_id, prev_attempts):
             else:
                 print(f'Max retries exceeded ({str(prev_attempts + 1)}) with tuition_id {str(tuition_id)}')
                 notify_admin(tuition_id)
-            return
-        # Process response
-        tuition_data = json.loads(tuition_request.text)
-        print(json.dumps(tuition_data, indent=4))
-    except Exception as error:
-        print(f'Error while requesting tuition data: {str(error)}')
-        return
-    # Check tuition status
-    if tuition_data['data']['estado_matricula'] == 'pendiente':
-        # Check is max attempts were reached
-        if prev_attempts < MAX_RETRIES - 1:
-            print('Scheduling next task...')
-            exec_date = datetime.utcnow() + timedelta(minutes=3)
-            check_tuition_status.apply_async((tuition_id, course_id, prev_attempts + 1), eta=exec_date)
         else:
-            print(f'Max retries exceeded ({str(prev_attempts + 1)}) with tuition_id {str(tuition_id)}')
-            notify_admin(tuition_id)
-    else:
-        # Send email to user
-        send_message(course_id, tuition_data['data']['email'], tuition_data['data']['usuario'], tuition_data['data']['password'], tuition_data['data']['nombre'], tuition_data['data']['apellidos'])
+            # Send email to user
+            send_message(course_id, tuition_data['data']['email'], tuition_data['data']['usuario'], tuition_data['data']['password'], tuition_data['data']['nombre'], tuition_data['data']['apellidos'])
 
 class Scheduler(Resource):
     def post(self):
         args = parser.parse_args()
         tuition_id = args['tuition_id']
         course_id = args['course_id']
+        # Creating log file
+        if not os.path.exists(LOGS_PATH):
+            os.mkdir(LOGS_PATH)
+        with open(LOGS_PATH + f'/{str(tuition_id)}.log', 'w', encoding='utf-8') as log_file:
+            log_file.write(f'Request date: {datetime.strftime(datetime.utcnow(), "%Y-%m-%dT%H:%M:%S")}\n')
         # Calling celery task
         exec_date = datetime.utcnow() + timedelta(minutes=1)
         try:
